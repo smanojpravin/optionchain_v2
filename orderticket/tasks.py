32d2fea92a8c563c795e2c6937be24daa6a4689c
smanojpravin <72971917+smanojpravin@users.noreply.github.com>
2023-03-13 08:55:15 +0530

    Update tasks.py

--- orderticket/tasks.py
+++ orderticket/tasks.py
@@ -472,13 +472,13 @@ def create_currency():
             HistoryOIChange.objects.filter(time__lte = pastDate).delete()
             HistoryOITotal.objects.filter(time__lte = pastDate).delete()
             HistoryOIPercentChange.objects.filter(time__lte = pastDate).delete()
-            HistoryVolume.objects.filter(time__lte = pastDate).delete()
+            #HistoryVolume.objects.filter(time__lte = pastDate).delete()
             
             # Deleting live data
             LiveOITotal.objects.filter(time__lte = pastDate).delete()
             LiveOIChange.objects.filter(time__lte = pastDate).delete()
             LiveOIPercentChange.objects.filter(time__lte = pastDate).delete()
-            LiveVolume.objects.filter(time__lte = pastDate).delete()
+            #LiveVolume.objects.filter(time__lte = pastDate).delete()
             # deleting past first data
     #         FirstLiveOITotal.objects.filter(time__lte = pastDate).delete()
     #         FirstLiveOIChange.objects.filter(time__lte = pastDate).delete()
